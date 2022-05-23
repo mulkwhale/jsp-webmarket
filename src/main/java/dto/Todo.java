30c3ea90674c3bd2743f87c5f0e38a631b9a85d6
mulkwhale <Kongyoonjung178@gmail.com>
2022-05-23 16:48:48 +0900

    mvc2 패턴 적용

--- src/main/java/dto/Todo.java
+++ src/main/java/dto/Todo.java
@@ -32,7 +32,7 @@ public class Todo {
 	public void setDone(boolean isDone) {
 		this.isDone = isDone;
 	}
-
+	
 	@Override
 	public String toString() {
 		return "Todo [id=" + id + ", task=" + task + ", isDone=" + isDone + "]";
